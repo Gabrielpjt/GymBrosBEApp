af3e3df16eae0dc81f0be0ae0a7a2a8df600f36c
Gabrielpjt01 <gabrielpanjaitan46@gmail.com>
2024-06-05 18:41:48 +0700

    Tambahin cors

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ import dotenv from 'dotenv';
 import ConnectionDB from './config/db.js';
 import ReportRouter from './routes/ReportRouter.js';
 import UserRouter from './routes/UserRouter.js';
+import cors from 'cors';
 import { protect } from './middleware/authMiddleware.js';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.options("*", cors());
+app.use(cors());
 
 // Default route
 app.get('/', (req, res) => {
